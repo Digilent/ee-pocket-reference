94985f0ca1d3ceb0bfad4b6f1b3ce3ac1b791ac3
Sudharsan Sukumar <SudharsanSukumar@github.com>
2016-05-12 11:18:29 -0700

    Simplify calcColor algorithm

--- app/services/resistor/resistor.service.ts
+++ app/services/resistor/resistor.service.ts
@@ -34,47 +34,27 @@ export class ResistorService {
     
     //Calculates colors from resistance value
     calcColor(resistanceValue: number) {
-        let resistanceString = resistanceValue.toString();
-        let length = resistanceString.length;
-        let decimalIndex = resistanceString.indexOf('.');
-        
-        //not done need to make smaller
-        if (decimalIndex === -1 && length > 1) {
-            //no dot
+        let resistanceString = Number(resistanceValue).toFixed(2);
+        //Color band is not gold or silver
+        if (resistanceValue >= 10) {
             this.color1 = this.colors[resistanceString[0]];
             this.color2 = this.colors[resistanceString[1]];
-            this.color3 = this.colors[(length - 2)];
-            this.resistance = resistanceValue;
+            this.color3 = this.colors[(resistanceString.length - 5)];
             return [this.color1, this.color2, this.color3];
         }
-        else if (decimalIndex == 0 || (decimalIndex == 1 && resistanceString[0] == '0')) {
-            if (length < 4) {
-                this.color1 = this.colors[resistanceString[length - 1]];
-                this.color2 = 'black';
-                this.color3 = 'silver';
-                return [this.color1, this.color2, this.color3];
-            }
-            else {
-                //round and silver
-                this.color1 = this.colors[resistanceString[2]];
-                this.color2 = this.colors[resistanceString[3]];
-                this.color3 = 'silver';
-                return [this.color1, this.color2, this.color3];
-            }
+        //Color band is gold
+        else if (resistanceValue < 10 && resistanceValue >= 1) {
+            this.color1 = this.colors[resistanceString[0]];
+            this.color2 = this.colors[resistanceString[2]];
+            this.color3 = 'gold';
+            return [this.color1, this.color2, this.color3]; 
         }
-        else if ((decimalIndex == 1 && resistanceString[0] != '0') || length == 1) {
-            if (length == 1) {
-                this.color1 = this.colors[resistanceString[0]];
-                this.color2 = 'black';
-                this.color3 = 'gold';
-                return [this.color1, this.color2, this.color3];
-            }
-            else {
-                this.color1 = this.colors[resistanceString[0]];
-                this.color2 = this.colors[resistanceString[2]];
-                this.color3 = 'gold';
-                return [this.color1, this.color2, this.color3]; 
-            }
+        //Color band is silver
+        else if (resistanceValue < 1) {
+            this.color1 = this.colors[resistanceString[2]];
+            this.color2 = this.colors[resistanceString[3]];
+            this.color3 = 'silver';
+            return [this.color1, this.color2, this.color3];
         }
         else {
             //error
